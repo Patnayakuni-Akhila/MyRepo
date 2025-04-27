0e58422a40a7bf2d38f1da92dfbb6302ca4769f6
patna <patna@LAPTOP-JHQ9HFKO.lan>
2025-04-27 20:20:36 +0530

    SpringBoot Application

---
+++ src/main/java/com/thinkbig/rest_d/RestDApplication.java
@@ -0,0 +1,13 @@
+package com.thinkbig.rest_d;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestDApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestDApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thinkbig/rest_d/RestDApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thinkbig.rest_d;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestDApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
